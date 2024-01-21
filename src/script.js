968346aa7cf8ad3543f2a0b28625c77bf9339ccc
melissadeleonx <primavita19@gmail.com>
2024-01-21 15:16:12 +0100

    index.html updated

--- src/script.js
+++ src/script.js
@@ -1,9 +1,2 @@
 
 // To replicate the expand feature of Google's search bar
-function autoExpandInput(input) {
-    input.style.height = 'auto';
-    input.style.height = (input.scrollHeight + 2) + 'px'; // Adding 2 pixels to prevent scroll bars
-    console.log('Input height adjusted:', input.style.height);
-}
-
-autoExpandInput(document.querySelector('.search-input'));
